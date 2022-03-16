16db2f10e42176613005ca76bc3c255f9e1c2d48
HareemManzoor <hareemmanzoor17@gmail.com>
2022-03-16 13:54:50 -0400

    Updated test cases

--- cypress/integration/Tests/webTableAsertion.spec.js
+++ cypress/integration/Tests/webTableAsertion.spec.js
@@ -12,9 +12,9 @@ describe(' Web Table Assertions',() => {
         cy.get('[aria-label="rows per page"]').should('have.value', '10')
     })
 
-    // it('Confirm there are multiple pages', () => {
-    //     cy.get('input[aria-label="jump to page"]').should('have.length.gt', '1')
-    // })
+    it('Confirm there are multiple pages', () => {
+        cy.get('input[aria-label="jump to page"]').should('have.length.gt', '1')
+    })
 
     it('Change pagination to show 20 pages', () => {
         cy.get('[aria-label="rows per page"]').select('20')
@@ -28,7 +28,7 @@ describe(' Web Table Assertions',() => {
         cy.get('.rt-tbody .rt-td:nth-child(3)')
         .then(($cells) => Cypress._.map($cells, 'innerText'))
         .then((strings) => Cypress._.map(strings, parseFloat))
-        .then((numbers) => cy.log(numbers.slice(0,3).join(', ')))
+        .then((numbers) => cy.log(numbers.slice(0,3).join(', '))) //will create shallow copy of array into new array numbers
         .should('not.be.sorted')
     })
     //For more https://www.youtube.com/watch?v=_vqOtLIGI9o
